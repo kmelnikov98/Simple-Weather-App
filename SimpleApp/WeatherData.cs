f2af76b00c18f2b8f29972b1d55efdc010113f19
kirill.melnikov <kirill.melnikov@avigilon.com>
2019-08-21 11:31:36 -0700

    Added

--- SimpleApp/WeatherData.cs
+++ SimpleApp/WeatherData.cs
@@ -9,8 +9,6 @@ namespace SimpleApp
 {
 	class WeatherData : IWeatherData
 	{
-
-
 		private string m_weatherData;
 		private const double k_tempZero = 273.15;
 
@@ -34,7 +32,7 @@ namespace SimpleApp
 		{
 			if (CityLocation != null && CountryLocation != null)
 			{
-				HttpResponse<string> jsonResponse = Unirest.get("https://community-open-weather-map.p.rapidapi.com/weather?callback=test&id=2172797&units=%22metric%22+or+%22imperial%22&mode=xml%2C+html&q=" + CityLocation + "%2C+" + CountryLocation)
+				HttpResponse<string> jsonResponse = Unirest.get("https://community-open-weather-map.p.rapidapi.com/weather?callback=test&id=2172797&units=%22metric%22+or+%22imperial%22&mode=xml%2C+html&q=" + CityLocation + "%2C+" + CountryLocation + "&units=%22metric%22")
 				.header("X-RapidAPI-Host", "community-open-weather-map.p.rapidapi.com")
 				.header("X-RapidAPI-Key", "baa31766f0mshf8829a0c8da5dd9p1a932ajsn480bfe440f3b")
 				.asJson<string>();
